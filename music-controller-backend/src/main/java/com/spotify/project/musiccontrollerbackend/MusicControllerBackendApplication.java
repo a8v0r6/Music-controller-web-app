2119ef31ea4e2d4e0c6dc84c9e283aec76083939
a8v0r6 <a8v0r6@protonmail.com>
2023-04-15 23:13:36 +0530

    Added project structure

---
+++ music-controller-backend/src/main/java/com/spotify/project/musiccontrollerbackend/MusicControllerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.spotify.project.musiccontrollerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicControllerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicControllerBackendApplication.class, args);
+	}
+
+}
---
+++ music-controller-backend/src/test/java/com/spotify/project/musiccontrollerbackend/MusicControllerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spotify.project.musiccontrollerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicControllerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
